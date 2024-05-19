701a06c09c4099b07c037709f46c2ea3a7973dc7
Ryan Zhang <horizon_157@outlook.com>
2024-05-20 00:03:07 +0800

    [add]: 增加百度千帆平台Llama3-70B模型调用

--- waf_ai.go
+++ waf_ai.go
@@ -14,9 +14,11 @@ import (
 )
 
 var gptKey string
+var aiType string
 
-func GPTInit(apikey string) {
+func GPTInit(apikey string, aitype string) {
 	gptKey = apikey
+	aiType = aitype
 }
 
 // ChatGPTRequest 表示发送到 ChatGPT API 的数据。
@@ -24,6 +26,10 @@ type ChatGPTRequest struct {
 	Model       string    `json:"model"`
 	Message     []Message `json:"messages"`
 	Temperature float32   `json:"temperature"`
+}
+
+type LlamaRequest struct {
+	Message []Message `json:"messages"`
 }
 
 type Message struct {
@@ -70,9 +76,38 @@ func ConstructionReq(request *WafHttpRequest) string {
 
 func AICheck(req *WafHttpRequest) {
 	checkString := ConstructionReq(req)
-	// 设置正确的 API 端点
-	url := "https://api.openai.com/v1/chat/completions"
-	// 创建一个新请求
+
+	fmt.Println(aiType)
+	if aiType == "ChatGPT" {
+		handleChatGPTRequest(req, checkString)
+	} else {
+		handleLlamaRequest(req, checkString)
+	}
+}
+
+func handleChatGPTRequest(req *WafHttpRequest, checkString string) {
+	chatGPTURL := "https://api.openai.com/v1/chat/completions"
+	reqData := createChatGPTRequest(checkString)
+	respData, err := sendRequest(chatGPTURL, reqData, true)
+	if err != nil {
+		log.Println("发送请求到 ChatGPT API 时出错:", err)
+		return
+	}
+	processResponse(req, respData)
+}
+
+func handleLlamaRequest(req *WafHttpRequest, checkString string) {
+	llamaURL := "https://aip.baidubce.com/rpc/2.0/ai_custom/v1/wenxinworkshop/chat/llama_3_70b?access_token=" + gptKey
+	reqData := createLlamaRequest(checkString)
+	respData, err := sendRequest(llamaURL, reqData, false) // 不需要使用Proxy
+	if err != nil {
+		log.Println("发送请求到 Llama API 时出错:", err)
+		return
+	}
+	processResponse(req, respData)
+}
+
+func createChatGPTRequest(checkString string) []byte {
 	reqData := ChatGPTRequest{
 		Message: []Message{
 			{
@@ -81,64 +116,121 @@ func AICheck(req *WafHttpRequest) {
 			},
 			{
 				Role:    "system",
-				Content: "You are an outstanding cybersecurity expert responsible for analyzing and identifying potential threats in data packets. You just need to answer me the attack category and credibility.format {\"attack category\": \"\", \"credibility\": \"low or middle or high\"}, If there is no attack, just reply to null.Only recognize SQL injection, XSS attack, Trojan injection, directory leakage, critical suffix file access, CSRF attack.",
+				Content: systemPrompt(),
 			},
 		},
 		Model:       "gpt-3.5-turbo",
 		Temperature: 0.7,
 	}
 	reqBody, _ := json.Marshal(reqData)
-	gptreq, _ := http.NewRequest("POST", url, bytes.NewBuffer(reqBody))
-	gptreq.Header.Set("Content-Type", "application/json")
-	gptreq.Header.Set("Authorization", "Bearer "+gptKey)
+	return reqBody
+}
 
-	// 创建一个 SOCKS5 代理拨号器
-	dialer, err := proxy.SOCKS5("tcp", "127.0.0.1:7898", nil, proxy.Direct)
+func createLlamaRequest(checkString string) []byte {
+	reqData := LlamaRequest{
+		Message: []Message{
+			{
+				Role:    "user",
+				Content: systemPrompt() + "\n" + checkString,
+			},
+		},
+	}
+	reqBody, _ := json.Marshal(reqData)
+	return reqBody
+}
+
+func sendRequest(url string, reqData []byte, useProxy bool) ([]byte, error) {
+	req, _ := http.NewRequest("POST", url, bytes.NewBuffer(reqData))
+	var client *http.Client
+	if useProxy {
+		req.Header.Set("Content-Type", "application/json")
+		req.Header.Set("Authorization", "Bearer "+gptKey)
+		dialer, err := proxy.SOCKS5("tcp", "192.168.110.15:7898", nil, proxy.Direct)
+		if err != nil {
+			return nil, err
+		}
+		client = &http.Client{
+			Transport: &http.Transport{
+				Dial: dialer.Dial,
+			},
+		}
+	} else {
+		req.Header.Set("Content-Type", "application/json")
+		req.Header.Set("Accept", "application/json")
+		client = &http.Client{}
+	}
+
+	resp, err := client.Do(req)
 	if err != nil {
-		log.Println("创建代理拨号器时出错:", err)
-		return
-	}
-
-	// 使用代理拨号器设置自定义 HTTP 传输
-	httpTransport := &http.Transport{
-		Dial: dialer.Dial,
-	}
-	client := &http.Client{
-		Transport: httpTransport,
-	}
-
-	// 通过代理发送请求
-	resp, err := client.Do(gptreq)
-	if err != nil {
-		log.Println("发送请求到 API 时出错:", err)
-		return
+		return nil, err
 	}
 	defer resp.Body.Close()
 
-	// 解析响应
-	body, _ := ioutil.ReadAll(resp.Body)
-	var chatGPTResponse ChatGPTResponse
-	_ = json.Unmarshal(body, &chatGPTResponse)
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	return body, nil
+}
 
-	// 获取 role 为 assistant 的消息
-	for _, choice := range chatGPTResponse.Choices {
-		if choice.Message.Role == "assistant" {
-			// 解析 content 字段为结构体
-			var contentData map[string]string
-			_ = json.Unmarshal([]byte(choice.Message.Content), &contentData)
-			fmt.Println(contentData["attack category"])
-			fmt.Println(contentData["credibility"])
-			if contentData["attack category"] != "null" || contentData["attack category"] != "" && contentData["credibility"] == "high" || contentData["credibility"] == "middle" {
-				WriteAiAttackCount(req.RemoteAddr)
-				ret := &WafProxyResp{
-					RetCode:  WAF_INTERCEPT,
-					RuleName: "AI LLM Check",
-					Desc:     contentData["attack category"],
-				}
-				// 生成UUID
-				id := uuid.New().String()
-				InsertEgAIAttackRecord(req, id, req.Host, ret.RuleName, ret.Desc)
-			}
+func systemPrompt() string {
+	return "You are an outstanding cybersecurity expert responsible for analyzing and identifying potential threats in data packets. You just need to answer me the attack category and credibility.format {\"attack category\": \"\", \"credibility\": \"low or middle or high\"}, If there is no attack, just reply to null.Only recognize SQL injection, XSS attack, Trojan injection, directory leakage, critical suffix file access, CSRF attack."
+}
+
+func processResponse(req *WafHttpRequest, response []byte) {
+	var responseData map[string]interface{}
+	err := json.Unmarshal(response, &responseData)
+	if err != nil {
+		log.Println("Error unmarshalling response:", err)
+		return
+	}
+	fmt.Println("responseData", responseData)
+
+	// 检查是否存在 'result' 字段，如果存在，直接从这个字段获取数据
+	if result, exists := responseData["result"].(string); exists {
+		var resultData map[string]string
+		err = json.Unmarshal([]byte(result), &resultData)
+		if err != nil {
+			log.Println("Error unmarshalling result content:", err)
+			return
 		}
+		processAttackInfo(req, resultData["attack category"], resultData["credibility"])
+	} else {
+		choices, ok := responseData["choices"].([]interface{})
+		if !ok || len(choices) == 0 {
+			log.Println("No choices available or invalid structure")
+			return
+		}
+
+		firstChoice := choices[0].(map[string]interface{})
+		message := firstChoice["message"].(map[string]interface{})
+		content := message["content"].(string)
+
+		// 解析 content 字符串到一个新的 map 中
+		var contentData map[string]string
+		err = json.Unmarshal([]byte(content), &contentData)
+		if err != nil {
+			log.Println("Error unmarshalling content:", err)
+			return
+		}
+		processAttackInfo(req, contentData["attack category"], contentData["credibility"])
+	}
+}
+
+// 处理攻击信息和生成响应
+func processAttackInfo(req *WafHttpRequest, attackCategory, credibility string) {
+	fmt.Println("Attack Category:", attackCategory)
+	fmt.Println("Credibility:", credibility)
+
+	if attackCategory != "null" && attackCategory != "" && (credibility == "high" || credibility == "middle") {
+		WriteAiAttackCount(req.RemoteAddr)
+		ret := &WafProxyResp{
+			RetCode:  WAF_INTERCEPT,
+			RuleName: "AI LLM Check",
+			Desc:     attackCategory,
+		}
+		// 生成UUID
+		id := uuid.New().String()
+		InsertEgAIAttackRecord(req, id, req.Host, ret.RuleName, ret.Desc)
 	}
 }
